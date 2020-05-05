f5105ed4c6b2d34bc86c0fe6f27424d16d91b6a9
lilirdz <lilimrod@gmail.com>
2020-05-05 13:37:42 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,25 @@
 class Deck
+    attr_reader :cards
+    
+    def initialize
+        @cards = []
+        for suit in ["Hearts", "Clubs", "Diamonds", "Spades"]
+            for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+                @cards << Card.new(suit,rank)
+            end
+        end
 
+    end
+
+    def choose_card
+        @cards.delete(@cards.sample)
+    end
 end
 
 class Card
-
+    attr_reader :suit, :rank
+    def initialize(suit,rank)
+    @suit = suit
+    @rank = rank
+    end
 end
